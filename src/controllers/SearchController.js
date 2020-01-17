cc887dc5ead4c48051c599c6a25606cfe72d92cd
Luiz Ferreira <luiz.ferreira@outlook.com>
2020-01-17 19:08:31 -0300

    Initial commit

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray =  parseStringAsArray(techs);
+        
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+        
+        console.log(techsArray);
+        // Buscar todos devs num raio de 10km
+        // Filtrar por tecnologias
+
+
+        return response.json({ devs });
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+require('dotenv').config()
+
+const app = express();
+console.log(process.env.MONGODB_CONN)
+
+mongoose.connect(process.env.MONGODB_CONN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+// Métodos HTTP: get, post, put, delete
+// Tipos de parâmetros:
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: req.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para a criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+app.listen(3333);
